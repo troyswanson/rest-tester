bd01507fa76e081ab0d4e16b2c0912314a626f36
Troy Swanson <troyswanson@me.com>
2013-09-16 23:11:08 -0500

    Remove Pictos, add a new GA tag

--- index.php
+++ index.php
@@ -4,19 +4,16 @@
 <title>The REST Tester</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link rel="stylesheet" type="text/css" href="css/global.css"/>
-<link rel="stylesheet" type="text/css" href="css/pictos.css"/>
 <script src="js/jquery-1.7.1.js"></script>
 <script src="js/index.js"></script>
-<script type="text/javascript">
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-20896597-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+  
+  ga('create', 'UA-44001982-1', 'resttester.co');
+  ga('send', 'pageview');
 </script>
 <script type="text/javascript" src="//use.typekit.net/zrf2poe.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
