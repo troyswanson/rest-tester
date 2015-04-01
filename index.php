63ccf4bb13d234c1e17c2c7db0ff8969fa329fc4
Troy Swanson <gerphimum@gmail.com>
2015-03-31 21:24:37 -0500

    Remove GA tag

--- index.php
+++ index.php
@@ -6,15 +6,6 @@
 <link rel="stylesheet" type="text/css" href="css/global.css"/>
 <script src="js/jquery-1.7.1.js"></script>
 <script src="js/index.js"></script>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  
-  ga('create', 'UA-44001982-1', 'resttester.co');
-  ga('send', 'pageview');
-</script>
 <script type="text/javascript" src="//use.typekit.net/zrf2poe.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 </head>
